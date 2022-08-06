5a923a9650773ff6ce92c1203abed3208d265e61
laurits598 <70940348+laurits598@users.noreply.github.com>
2022-08-06 21:09:33 +0200

    Update dataElements.py

--- dataElements.py
+++ dataElements.py
@@ -1,38 +1,42 @@
-import pandas as pd
-path = "C:/Users/Laurits/Desktop/Kemi/Grundstoffer.csv"
+import os
+from PIL import Image
+import PySimpleGUI as sg
+from os.path import exists
+import periodictable as pt
+import numpy as np
+
+directory = "C:/Users/Laurits/Desktop/Kemi/elementsImages/"
 
 df = pd.read_csv(path, delimiter=';')
 
-def getInfo(char):
-	for i in range(0, len(df)):
-		if df.iloc[i][0] == char:
-			print("Name:   ", df.iloc[i][1])
-			print("Number: ", df.iloc[i][2])
-			print("Mass:   ", df.iloc[i][3], "g/mol")
-			return df.iloc[i][1:3]
-	print("not valid")
+Gs = [[]]
+for x in range(1,119):
+	Gs.append([[x], pt.elements[x].name, pt.elements[x], pt.elements[x].mass])
+	#print(x)
 
-def getMass(char):
-	for i in range(0, len(df)):
-		if df.iloc[i][0] == char:
-			print(df.iloc[i][3])
-			return df.iloc[i][3]
-	print("not valid")
+def getMass(num):
+	return pt.elements[num].mass
 
-def getName(char):
-	for i in range(0, len(df)):
-		if df.iloc[i][0] == char:
-			print(df.iloc[i][1])
-			return df.iloc[i][1]
-	print("not valid")
+def getName(num):
+	name = pt.elements[num].name
+	name = name.capitalize()
+	return name
 
 def getNumber(char):
-	for i in range(0, len(df)):
-		if df.iloc[i][0] == char:
-			print(df.iloc[i][2])
-			return df.iloc[i][2]
-	print("not valid")
+	for i in range(1, 119):
+		if char == str(Gs[i][2]):
+			print(i)
+			return i
+	
 
-
-
+def getImage(num):
+	print("inputtet num: ", num)
+	num = int(num)
+	name = getName(num)
+	print("name: ", name)
+	file = directory + name + ".png"
+	print("file: ",file)
+	#img  = Image.open(file) 
+	#img.show()
+	return file
 
